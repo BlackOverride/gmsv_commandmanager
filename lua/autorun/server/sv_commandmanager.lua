22d0dc1d0c4bb2cb0758850c4295bf879a36f593
Linda Black <goonipkaywoods@gmail.com>
2025-11-28 07:05:35 +0200

    Update sv_commandmanager.lua
    Fixed BlockCommand was removing commands from console
    A better approach to know if its console execute

--- lua/autorun/server/sv_commandmanager.lua
+++ lua/autorun/server/sv_commandmanager.lua
@@ -1,22 +1,47 @@
-if not pcall(require, "commandmanager") then
-    print("CommandManager plugin not found, you can ignore this if you dont want this plugin.")
-    return
+if not _G.CommandManagerLoaded then
+    _G.CommandManagerLoaded = true
+    if not pcall(require, "commandmanager") then
+        print("CommandManager plugin not found, you can ignore this if you dont want this plugin.")
+        return
+    end
+end
+
+-- wrapper to create Lua command that checks player vs console
+_RemoveCommand = _RemoveCommand or BlockCommand -- save our original callback first
+function BlockCommand(cmdName)
+    _RemoveCommand(cmdName)
+
+    concommand.Add(cmdName, function(ply, cmd, args, argStr)
+        if IsValid(ply) then ply:PrintMessage(HUD_PRINTCONSOLE, "Unknown command: " .. cmdName) return end -- if its a player then we dont want it so return early
+        ExecuteBlockedCommand(cmdName, argStr or "") -- call C++ to execute the original command
+    end)
+end
+
+function RemoveCommand(cmdName)
+    _RemoveCommand(cmdName)
+    concommand.Add(cmdName, function(ply, cmd, args, argStr)
+        if IsValid(ply) then ply:PrintMessage(HUD_PRINTCONSOLE, "Unknown command: " .. cmdName) return -- if its a player then we dont want it so return early
+		else print(string.format('Unknown command: "%s"', cmdName)) return end
+    end)
 end
 
 -- block the 'status' command (only console can run this)
 BlockCommand("status")
 
--- unblock the 'status' command (players can use it again on their console)
---UnblockCommand("status")
+-- unblock the 'status' command (players can use it again)
+--RestoreCommand("status")
 
--- remove the 'lua_dumptimers_sv' command (cannot be run on server anymore even by console)
-RemoveCommand("lua_dumptimers_sv")
+-- to completely remove a command
+--RemoveCommand("lua_dumptimers_sv")
 
--- restore the 'lua_dumptimers_sv' command we deleted
---RestoreCommand("lua_dumptimers_sv")
+-- restore removed command
+-- RestoreCommand("lua_dumptimers_sv")
 
--- remove cheat flag from command so we can use it without sv_cheats 1 (this will only work on commands that can be executed by server no on client realm.)
+-- completely remove a command with no way to restore!
+_RemoveCommand("lua_dumptimers_sv")
+
+-- remove cheat flag from command (server-side commands only)
 --UncheatCommand("sv_showlagcompensation")
 
--- put the cheat flag back on command (requires sv_cheats 1 again)
+-- restore cheat flag
 --RecheatCommand("sv_showlagcompensation")
